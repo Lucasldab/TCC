22c783d8989ca847ef8de806c4c4a4ca9e219462
Lucas Barros <lucasldabarros@gmail.com>
2023-10-29 23:41:39 -0300

    Updated CNN for new sampling methods

--- CNN.py
+++ CNN.py
@@ -10,16 +10,31 @@ from keras import layers
 from keras import models
 from keras import losses
 from optimizer_selector import randomize_optimizer
+import Sampling
 
+trains = int(input("CNN numbers: "))
 
-if not os.path.exists('data/training_CNN_results_v3.csv'):
-     with open('data/training_CNN_results_v3.csv', 'w', newline='') as file:
+if not os.path.exists('data/teste.csv'):
+     with open('data/teste.csv', 'w', newline='') as file:
           writer = csv.writer(file)
           writer.writerow(["Name","Convoluted_Layers1","Convoluted_Filters1","Convoluted_Layers2","Convoluted_Filters2","Hidden_Layer1", "Hidden_Layer2", "Learning_Rate","Batch_Size","Loss"])
           file.close()
 
-for training in range(0,1000,1):
-    print("Training: ",training)
+conv_n1 = Sampling.lhs_sampling(trains,16.,32.)
+conv_f1 = Sampling.lhs_sampling(trains,1., 3.)
+
+conv_n2 = Sampling.lhs_sampling(trains,16., 32.)
+conv_f2 = Sampling.lhs_sampling(trains,1., 3.)
+
+L1 = Sampling.lhs_sampling(trains,32., 64.)
+L2 = Sampling.lhs_sampling(trains,16., 32.)
+
+optimizer_number = Sampling.lhs_sampling(trains,1., 9.)
+l_rate = Sampling.lhs_sampling(trains,0.0001, 0.01, round=False)
+bt_size = Sampling.lhs_sampling(trains,32., 128.)
+
+for training in range(0,trains,1):
+    print("Training: ",training+1)
     (x_train, y_train), (x_test, y_test) = mnist.load_data()
 
     x_train = x_train.reshape((60000,28,28,1))
@@ -33,34 +48,29 @@ for training in range(0,1000,1):
 
     model = models.Sequential()
 
-    conv_n1 = random.randint(16, 32)
-    conv_f1 = random.randint(1, 3)
-    model.add(layers.Conv2D(conv_n1,(conv_f1), activation='relu', input_shape = (28,28,1)))
+    model.add(layers.Conv2D(int(conv_n1[training]),(int(conv_f1[training])), activation='relu', input_shape = (28,28,1)))
     model.add(layers.MaxPooling2D((2,2)))
-    conv_n2 = random.randint(16, 32)
-    conv_f2 = random.randint(1, 3)
-    model.add(layers.Conv2D(conv_n2,(conv_f2), activation='relu'))
+
+    model.add(layers.Conv2D(int(conv_n2[training]),(int(conv_f2[training])), activation='relu'))
     model.add(layers.MaxPooling2D((2,2)))
     model.add(layers.Flatten())
-    L1 = random.randint(32, 64)
-    model.add(layers.Dense(L1,activation = 'relu'))
-    L2 = random.randint(16, 32)
-    model.add(layers.Dense(L2,activation = 'relu'))
+
+    model.add(layers.Dense(L1[training],activation = 'relu'))
+    model.add(layers.Dense(L2[training],activation = 'relu'))
     model.add(layers.Dense(10, activation= 'softmax'))
 
     #model.summary()
 
-    optimizer, l_rate, name = randomize_optimizer()
+    optimizer, name = randomize_optimizer(optimizer_number[training],l_rate[training])
 
     model.compile(optimizer = optimizer, loss = 'categorical_crossentropy', metrics = ['accuracy'])
-    bt_size = random.randint(32, 128)
     epoc=5
-    history = model.fit(x_train, y_train, epochs=epoc, batch_size = bt_size )
+    history = model.fit(x_train, y_train, epochs=epoc, batch_size = bt_size[training] )
 
     history_dict = history.history
-    print("Optmizer:{} Convoluted Layers 1:{} Convoluted Filters 1:{} Convoluted Layers 2:{} Convoluted Filters 2:{}  Hidden Layer 1:{} Hidden Layer 2:{} Learning Rate:{} Batch Size:{} Loss:{}".format(name,conv_n1,conv_f1,conv_n2, conv_f2,L1,L2,l_rate,bt_size,history_dict['loss'][epoc-1]))
+    print("Optmizer:{} Convoluted Layers 1:{} Convoluted Filters 1:{} Convoluted Layers 2:{} Convoluted Filters 2:{}  Hidden Layer 1:{} Hidden Layer 2:{} Learning Rate:{} Batch Size:{} Loss:{}".format(name,conv_n1[training],conv_f1[training],conv_n2[training], conv_f2[training],L1[training],L2[training],l_rate[training],bt_size[training],history_dict['loss'][epoc-1]))
 
-    with open('data/training_CNN_results_v3.csv', 'a', newline='') as file:
+    with open('data/teste.csv', 'a', newline='') as file:
          writer = csv.writer(file)
-         writer.writerow([name,conv_n1,conv_f1,conv_n2, conv_f2,L1,L2,l_rate,bt_size,history_dict['loss'][epoc-1]])
+         writer.writerow([name,conv_n1[training],conv_f1[training],conv_n2[training], conv_f2[training],L1[training],L2[training],l_rate[training],bt_size[training],history_dict['loss'][epoc-1]])
          file.close()
